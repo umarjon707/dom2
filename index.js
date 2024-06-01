a211a125552883b0bf8f90a4bc417f28864909b8
Umarjon <urr777777@gmail.com>
2024-06-01 15:02:51 +0500

    fixed

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const butn = document.querySelector('.colors')
-const body = document.querySelector('body')
-
-const img =document.querySelector('.imgBox') 
-
-function changeImage(){
-  
-    const iphpro = document.querySelector('.iphpro')
-    const colors = iphpro
-    const wh = document.querySelector('.colors2')
-    if(iphpro) {
-        iphpro.src='./img/blueiphone.png'
-    } else if(colors) {
-        wh.src ='./img/whiteiphone.webp'
+const imageInput = document.getElementById('backgroundImageInput');
+imageInput.addEventListener('change', (event) => {
+const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader(); 
+    reader.onload = (e) => { 
+      document.body.style.backgroundImage = `url(${e.target.result})`;
     }
-   
-  
-}
-
-console.log(butn,body,iphpro,img);
+    reader.readAsDataURL(file);
+  }
+});
